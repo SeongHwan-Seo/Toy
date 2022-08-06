6901e9ce48266bbb2cf2c316f8d6c4d42ee934b1
SeongHwan-Seo <seosh5704@gmail.com>
2022-08-06 13:43:33 +0900

    init commit

---
+++ Covid19/Covid19/Shared/Covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19App.swift
+//  Shared
+//
+//  Created by SHSEO on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct Covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
