c5186bf095eb64fb088cc4811ce1476970ba4ce9
SeongHwan-Seo <seosh5704@gmail.com>
2023-04-18 16:21:29 +0900

    Feat: TCA Client 생성

--- Toy-OpenChat2/Toy-OpenChat2/Client/MessageClient.swift
+++ Toy-OpenChat2/Toy-OpenChat2/Client/MessageClient.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import ComposableArchitecture
+import Starscream
+
+struct MessageClient {
+    var connect: () -> EffectPublisher<Void, Error>
+    var sendMessage: (_ msg: String) -> EffectPublisher<Void, Error>
+    
+    struct Failure: Error, Equatable {}
+}
+
+extension MessageClient {
+    static let live = MessageClient( connect: {
+        SocketManager.shared
+            .connect()
+            .eraseToEffect()
+    }, sendMessage: { msg in
+        SocketManager.shared
+            .sendMessage(msg)
+            .eraseToEffect()
+    })
+}
