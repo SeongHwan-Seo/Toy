bc6d5e19b4151856eec1950f6725ee07bd28709e
SeongHwan-Seo <seosh5704@gmail.com>
2023-04-27 11:02:56 +0900

    Refactir: VStack -> LazyVStack 으로 변경

--- Toy-OpenChat/Toy-OpenChat/Chat/View/ChatView.swift
+++ Toy-OpenChat/Toy-OpenChat/Chat/View/ChatView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ChatView: View {
+    let name: String
     @State private var messageText = ""
     @StateObject var viewModel = ChatVM()
     
@@ -16,12 +17,12 @@ struct ChatView: View {
             VStack {
                 ScrollViewReader { proxy in
                     ScrollView {
-                        VStack(spacing: 10) {
+                        LazyVStack(spacing: 10) {
                             ForEach(viewModel.message, id: \.id) { message in
-                                MessageView(message: message)
+                                MessageView(message: message, name: name)
                             }
                         }
-                        //.frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .padding()
                         .onChange(of: viewModel.scrollToEnd, perform: { value in
                             if value {
@@ -41,20 +42,20 @@ struct ChatView: View {
                     TextField("메시지 보내기...", text: $messageText)
                     Button("Send") {
                         if messageText == "" { return }
-                        viewModel.sendMessage(messageText)
+                        viewModel.sendMessage(messageText, name)
                         messageText = ""
                     }
                 }
                 .padding()
                 
             }
-            //.navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitleDisplayMode(.inline)
         
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
-        ChatView()
+        ChatView(name: "")
     }
 }
--- Toy-OpenChat/Toy-OpenChat/Chat/View/MessageView.swift
+++ Toy-OpenChat/Toy-OpenChat/Chat/View/MessageView.swift
@@ -10,17 +10,25 @@ import Kingfisher
 
 struct MessageView: View {
     let message: MessageData
-    let myName = UserDefaults.standard.string(forKey: "name")
+    let name: String
     
     var body: some View {
         HStack {
-            if message.username ==  myName{
-                Spacer()
-                makeBallonView(foregroundColor: .white, backgroundColor: .mainColor, corners: [.topLeft, .topRight, .bottomLeft])
+            if message.username ==  name{
+                HStack(spacing: 0) {
+                    Spacer()
+                    makeTime()
+                        .padding(.trailing, 4)
+                        .padding(.bottom, 1)
+                    makeBallonView(foregroundColor: .white, backgroundColor: .mainColor, corners: [.topLeft, .topRight, .bottomLeft])
+                }
+                
             } else {
                 HStack {
                     makeProfileView()
                     makeBallonView(foregroundColor: Color.black, backgroundColor: Color(.lightGray), corners: [.topLeft, .topRight, .bottomRight])
+                    makeTime()
+                        .padding(.bottom, 1)
                 }
                 
                 Spacer()
@@ -49,12 +57,58 @@ struct MessageView: View {
             .background(backgroundColor)
             .cornerRadius(15, corners: corners)
     }
+    
+    @ViewBuilder
+    private func makeDate(dateString: String) -> some View {
+        Text(dateString)
+            .font(.system(size: 13, weight: .light))
+            .foregroundColor(Color.black)
+    }
+    
+    @ViewBuilder private func makeTime() -> some View {
+        VStack(alignment: message.username ==  name ? .trailing : .leading, spacing: 0) {
+            Spacer()
+            Text(getRelativeTimeString(from: message.createdAt!) ?? "")
+                .font(.system(size: 11, weight: .light))
+                .foregroundColor(Color.black)
+        }
+    }
+    
+    private func getRelativeTimeString(from dateString: String) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let date = dateFormatter.date(from: dateString) else {
+            return nil
+        }
+        
+        var calendar = Calendar.current
+        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
+        let now = Date()
+        
+        if let minuteDiff = calendar.dateComponents([.minute], from: date, to: now).minute, minuteDiff <= 1 {
+            return "방금"
+        } else if let hourDiff = calendar.dateComponents([.hour], from: date, to: now).hour, hourDiff <= 1 {
+            let minuteDiff = calendar.dateComponents([.minute], from: date, to: now).minute ?? 0
+            return "\(minuteDiff)분전"
+        } else if let dayDiff = calendar.dateComponents([.day], from: date, to: now).day, dayDiff <= 1 {
+            let hourDiff = calendar.dateComponents([.hour], from: date, to: now).hour ?? 0
+            return "\(hourDiff)시간전"
+        } else if let monthDiff = calendar.dateComponents([.month], from: date, to: now).month, monthDiff <= 1 {
+            let dayDiff = calendar.dateComponents([.day], from: date, to: now).day ?? 0
+            return "\(dayDiff)일전"
+        } else if let yearDiff = calendar.dateComponents([.year], from: date, to: now).year, yearDiff <= 1 {
+            let monthDiff = calendar.dateComponents([.month], from: date, to: now).month ?? 0
+            return "\(monthDiff)달전"
+        } else {
+            let yearDiff = calendar.dateComponents([.year], from: date, to: now).year ?? 0
+            return "\(yearDiff)년전"
+        }
+    }
+    
 }
-
-
 
 struct MessageView_Previews: PreviewProvider {
     static var previews: some View {
-        MessageView(message: MessageData())
+        MessageView(message: MessageData(), name: "")
     }
 }
